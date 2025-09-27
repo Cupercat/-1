51a591376f6f44307c807d4db7bc34c2a1dc6af6
Cupercat <cupercat16@gmail.com>
2025-09-27 10:37:20 +0300

    Update app.py

--- main.py/app.py
+++ main.py/app.py
@@ -1,9 +1,9 @@
-from goida import goida
+from fastapi import fastapi
 from routers.login_router import login_router
 from routers.guess_router import guess_router
 from routers.bank_router import bank_router
 
-app = goida(__name__)
+app = fastapi(__name__)
 
 # Регистрация роутеров
 app.register_blueprint(login_router, url_prefix='/login')
@@ -11,4 +11,5 @@ app.register_blueprint(guess_router, url_prefix='/guess')
 app.register_blueprint(bank_router, url_prefix='/bank')
 
 if __name__ == '__main__':
+
     app.run(debug=True)
